bf31fad1b976948ea70e28e57ad4a76c0b7edca2
erandakaanupama <erandakaanupama@gmail.com>
2019-06-17 10:58:57 +0530

    common view component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommonViewComponent } from './common-view/common-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommonViewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/common-view/common-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-common-view',
+  templateUrl: './common-view.component.html',
+  styleUrls: ['./common-view.component.css']
+})
+export class CommonViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
